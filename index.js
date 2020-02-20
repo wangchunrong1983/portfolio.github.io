b34f07b414c86e43cf6fc85ea8a90158714b7ef4
Chunrong <56704548+wangchunrong1983@users.noreply.github.com>
2020-02-19 20:12:10 -0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const barbara = document.querySelector('.barbara')
 const slider = document.querySelector('.slider')
-const logo = document.querySelector('#logo')
-const menu = document.querySelector('.menu')
-const headline = document.querySelector('.headline')
+const leftArm = document.querySelector('#Leftarm')
+const rightArm = document.querySelector('#Rightarm')
 
 const t1 = new TimelineMax();
 
-t1.fromTo(barbara, 2, {height: "0%"}, {height: "80%", ease: Sine.easeInOut})
-  .fromTo(barbara, 2, {width: "100%"}, {width: "80%", ease: Sine.easeInOut})
+t1.fromTo(barbara, 1, {height: "0%"}, {height: "80%", ease: Sine.easeInOut})
+  .fromTo(barbara, 1, {width: "100%"}, {width: "100%", ease: Sine.easeInOut})
   .fromTo(slider, 2, {x: "-100%"}, {x: "0%", ease: Sine.easeInOut}, "-=2")
-  .fromTo(logo, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
-  
+  .to(leftArm,2, {left:100})
+  .to(rightArm, 2, {right:200});
+
+
+  window.sr = ScrollReveal();
+  sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 200
+  })
+
+  sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 400
+  })
+
+  sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 400
+  })
+
+  sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 400
+  })
+
